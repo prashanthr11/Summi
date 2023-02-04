b9e2a0e567524eeb2be1a64ffcaf82e6daede8e1
Prashanth <prashanthkumarreddy.busupal523@myci.csuci.edu>
2023-02-04 12:47:16 -0800

    created repo

---
+++ summI/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for summI project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "summI.settings")
+
+application = get_wsgi_application()
