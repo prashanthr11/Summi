f9fd3a07b44e0d078f73733e6d8db8c7c418cc37
Prashanth <prashanthkumarreddy.busupal523@myci.csuci.edu>
2023-02-19 16:33:17 -0800

    added secret key in env var

--- summI/settings.py
+++ summI/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from decouple import config
 from pathlib import Path
-from os import path, makedirs
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
