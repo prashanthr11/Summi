dc7a9c4df8213c0ea6278f275aeaadb1411cb32f
Prashanth <prashanthkumarreddy.busupal523@myci.csuci.edu>
2023-02-21 13:51:26 -0800

    fixed writing to a file in string format rather than binary

--- summIApp/views.py
+++ summIApp/views.py
@@ -5,8 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 import logging
 from .utils import *
-from summI.settings import MEDIA_PATH
+from summI.settings import MEDIA_PATH, MEDIA_URL
 from .constants import *
+from PIL import Image
 
 # logging
 logger = logging.getLogger("django")
@@ -21,6 +22,7 @@ def UserUploadedFilesView(request):
             user = request.user
             uploaded_file = request.FILES['uploaded_file']
             is_valid_file = False
+            is_public_file = True
 
             if uploaded_file.size > max_file_size:
                 return JsonResponse({
@@ -40,6 +42,7 @@ def UserUploadedFilesView(request):
 
             if user.is_authenticated:
                 user = User.objects.filter(user=user).first()
+                is_public_file = False
             else:
                 user = User.objects.filter(username="guest_user")
                 if not len(user):
@@ -51,14 +54,21 @@ def UserUploadedFilesView(request):
                     user = user.first()
 
             uploaded_file_object = UserUploadedFiles.objects.create(
-                user=user, file_name=file_name)
+                user=user, file_name=file_name, is_public_file=is_public_file)
             file_path = os.path.join(
                 MEDIA_PATH, str(uploaded_file_object.uuid))
 
             if create_dirs(file_path):
                 file_path = os.path.join(file_path, file_name)
-                with open(file_path, "wb") as f:
-                    f.write(uploaded_file.file.read())
+                try:
+                    tmp_image = Image.open(uploaded_file.file)
+                    tmp_image.save(file_path)
+                except Exception as e:
+                    logger.error(str(e))
+                    return JsonResponse({
+                        "status": 301,
+                        "message": "Cannot able to save the file to the disk",
+                    })
 
                 uploaded_file_object.file_path = file_path
                 uploaded_file_object.save()
@@ -72,6 +82,7 @@ def UserUploadedFilesView(request):
                 "status": 200,
                 "message": "success",
                 "image_id": str(uploaded_file_object.uuid),
+                "image_url": MEDIA_URL + str(uploaded_file_object.uuid) + "/" + str(uploaded_file_object.file_name),
             })
         except Exception as e:
             logger.error(str(e))
