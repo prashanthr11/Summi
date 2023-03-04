befc7528da2bff42cffae5f3046cc4e544571b4d
Sarakhoshnoudrad <123023662+Sarakhoshnoudrad@users.noreply.github.com>
2023-03-03 17:13:25 -0800

    Update webptojpeg.py

--- summIApp/ocr_model/webptojpeg.py
+++ summIApp/ocr_model/webptojpeg.py
@@ -1,6 +1,5 @@
 import PIL
 import os
-import imghdr
 
 def convert_webp_to_png(webp_file):
   # Open the webp image using PIL.Image.open()
@@ -11,11 +10,3 @@ def convert_webp_to_png(webp_file):
   png_file = file_name + '.png'
   # Save the image as png using PIL.Image.save()
   im.save(png_file, format="png", lossless=True)
-
-# a sensor for detecting any 
-def webp_sensor(Uploaded_image):
-  image_type = imghdr.what(Uploaded_image)
-  if  image_type == 'webp':
-    return convert_webp_to_png(Uploaded_image)
-  else:
-    return None
